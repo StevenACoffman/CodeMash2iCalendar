ff7f8e41739c6be041e573507f7bc0ffed1465cf
Steve Coffman <steve.coffman@ithaka.org>
2018-01-02 15:46:14 -0500

    Initial work... still stuff to do before working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CodeMash2iCalendar
 Utility to convert the CodeMash Session Schedule json into iCalendar (ICS)
+
+
+### Generate input files
+
+Hrmm... `curl -H "Content-Type: application/json" https://cmprod-speakers.azurewebsites.net/api/sessionsdata | jq .` gets the Codemash Session list.
+
+### Convert Input files to ICS files 
+
+Node [ICS](https://github.com/adamgibbons/ics) library has a nifty little [wiki page on creating multiple events](https://github.com/adamgibbons/ics/wiki/Creating-multiple-events).
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//import ics from 'ics';
+//import uuidv4 from 'uuid/v4';
+//import fs from 'fs';
+//import {promisify} from 'util';
+
+const {promisify} = require('util');
 const ics = require('ics')
 const fs = require('fs')
 const uuidv4 = require('uuid/v4')
+
+const readFileAsync = promisify(fs.readFile);
 
 const events = [
   {
@@ -17,6 +25,7 @@ const events = [
   }
 ]
 
+/*
 events.map((event) => {
   ics.createEvent(event, (error, value) => {
     if (error) throw error
@@ -26,3 +35,32 @@ events.map((event) => {
     })
   })
 })
+*/
+
+
+
+
+
+async function readInput() {
+  let names;
+  try {
+    text = await readFileAsync('codemash.json', {encoding: 'utf8'});
+    if (err) {
+        // Handle the error.
+    }
+  } catch (e) {
+    console.log('Reading file got error', e);
+  }
+  if (text === undefined) {
+    console.log('CONTENT is undefined');
+  } else {
+    console.log('CONTENT:', text));
+  }
+}
+
+readInput();
+// Next steps:
+// JSON.parse text
+// Convert object to event format
+// Write files to events directory using commented code
+// Get fancy with promisify, Async Await, write tests, or whatever 
