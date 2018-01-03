d37961c3a221072f199d330b950fed4d33195753
Matthew Morgan <matt@dulcetsoftware.com>
2018-01-03 07:19:17 -0500

    Implement mapping of event JSON to ICS input format

--- index.js
+++ index.js
@@ -3,62 +3,90 @@
 //import fs from 'fs';
 //import {promisify} from 'util';
 
-const {promisify} = require('util');
+const {promisify} = require('util')
 const ics = require('ics')
 const fs = require('fs')
 const uuidv4 = require('uuid/v4')
 
-const readFileAsync = promisify(fs.readFile);
+const readFileAsync = promisify(fs.readFile)
 
-const events = [
-  {
-    title: 'First event',
-    start: [2018, 5, 30, 6, 30],
-    duration: { hours: 1 },
-    uid: uuidv4() // generate a unique ID
-  },
-  {
-    title: 'Second event',
-    start: [2018, 6, 30, 6, 30],
-    duration: { minutes: 30 },
-    uid: uuidv4()
-  }
-]
+// const events = [
+//   {
+//     title:    'First event',
+//     start:    [2018, 5, 30, 6, 30],
+//     duration: {hours: 1},
+//     uid:      uuidv4() // generate a unique ID
+//   },
+//   {
+//     title:    'Second event',
+//     start:    [2018, 6, 30, 6, 30],
+//     duration: {minutes: 30},
+//     uid:      uuidv4()
+//   }
+// ]
+//
+// events.map((event) => {
+//   ics.createEvent(event, (error, value) => {
+//     if (error) throw error
+//
+//     fs.writeFile(`events/${event.uid}.ics`, value, (error) => {
+//       if (error) throw error
+//     })
+//   })
+// })
 
-/*
-events.map((event) => {
-  ics.createEvent(event, (error, value) => {
-    if (error) throw error
-
-    fs.writeFile(`events/${event.uid}.ics`, value, (error) => {
-      if (error) throw error
-    })
-  })
-})
-*/
-
+function extractTime(timeString) {
+  let date = new Date(timeString)
+  return [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getHours() + 1,
+    date.getMinutes() + 1
+  ]
+}
 
 
-
-
-async function readInput() {
-  let names;
-  try {
-    text = await readFileAsync('codemash.json', {encoding: 'utf8'});
-    if (err) {
-        // Handle the error.
-    }
-  } catch (e) {
-    console.log('Reading file got error', e);
-  }
-  if (text === undefined) {
-    console.log('CONTENT is undefined');
-  } else {
-    console.log('CONTENT:', text));
+function mapObjToEvent(obj) {
+  return {
+    title: obj.Title,
+    start: extractTime(obj.SessionStartTime),
+    end:   extractTime(obj.SessionEndTime),
+    uid:   uuidv4()
   }
 }
 
-readInput();
+async function readInput() {
+  let names
+  try {
+    text = await readFileAsync('codemash.json', {encoding: 'utf8'})
+  } catch (e) {
+    console.log('Reading file got error', e)
+  }
+  if (text === undefined) {
+    console.log('CONTENT is undefined')
+  } else {
+    return JSON.parse(text)
+  }
+}
+
+const writeEvent = (error, value) => {
+  if (error) throw error
+
+  fs.writeFile(`events/${value.uid}.ics`, value, (error) => {
+    if (error) throw error
+  })
+}
+
+async function doIt() {
+  let allObjects = await readInput()
+  let events = allObjects
+      .map(mapObjToEvent)
+
+  console.log(events[0])
+}
+
+doIt()
 // Next steps:
 // JSON.parse text
 // Convert object to event format
